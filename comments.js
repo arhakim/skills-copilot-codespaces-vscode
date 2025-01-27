bbc68629d7b80fe06eefab6a02e36204cce0cdfc
Andri Rahman Hakim <arhakim.512@gmail.com>
2025-01-27 07:13:38 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,11 @@
-// create web server
+// Create web server
 const express = require('express');
 const app = express();
 const port = 3000;
-
 // create a route
 app.get('/comments', (req, res) => {
     res.send('Comments');
 });
-
 // start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
